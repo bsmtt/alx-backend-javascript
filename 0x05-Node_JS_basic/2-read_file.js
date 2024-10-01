047eb36d6972d2652d8d9f1fd3a99d0af98473f6
bsm2 <basmatalaat2@gmail.com>
2024-10-01 22:45:33 +0300

    database

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+function filterByGroup(group, dataset) {
+  return dataset.filter((entry) => entry.split(',').at(-1).toString() === group);
+}
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const sData = data.toString().trim().split('\n');
+    const fullData = sData.splice(1, data.length);
+    console.log(`Number of students: ${fullData.length}`);
+    const groups = new Set(fullData.map((item) => item.split(',').at(-1)));
+    for (const group of groups) {
+      const filtered = filterByGroup(group, fullData);
+      const first_names = filtered.map((entry) => entry.split(',').at(0));
+      console.log(`Number of students in ${group}: ${filtered.length}. List: ${first_names.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
