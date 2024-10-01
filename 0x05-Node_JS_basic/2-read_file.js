84b40c956f41f21570eaadc3155852b6f9dff7bc
bsm2 <basmatalaat2@gmail.com>
2024-10-01 23:07:30 +0300

    database

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const displayMessage = require('./0-console');
 
 function filterByGroup(group, dataset) {
-  return dataset.filter((entry) => entry.split(',').at(-1).toString() === group);
+  return dataset.filter((entry) => entry.split(',').at(-1).toString().trim() === group);
 }
 
 function countStudents(path) {
@@ -9,12 +10,14 @@ function countStudents(path) {
     const data = fs.readFileSync(path, 'utf-8');
     const sData = data.toString().trim().split('\n');
     const fullData = sData.splice(1, data.length);
-    console.log(`Number of students: ${fullData.length}`);
-    const groups = new Set(fullData.map((item) => item.split(',').at(-1)));
+
+    displayMessage(`Number of students: ${fullData.length}`);
+
+    const groups = new Set(fullData.map((item) => item.trim().split(',').at(-1)));
     for (const group of groups) {
       const filtered = filterByGroup(group, fullData);
-      const first_names = filtered.map((entry) => entry.split(',').at(0));
-      console.log(`Number of students in ${group}: ${filtered.length}. List: ${first_names.join(', ')}`);
+      const firstNames = filtered.map((entry) => entry.split(',').at(0));
+      displayMessage(`Number of students in ${group}: ${filtered.length}. List: ${firstNames.join(', ')}`);
     }
   } catch (err) {
     throw new Error('Cannot load the database');
