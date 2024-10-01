4ff1cddd08da334678386aad40efa77f652c29a1
bsm2 <basmatalaat2@gmail.com>
2024-10-01 21:38:10 +0300

    feat: console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {string} m 
+ * @returns {void}
+ */
+const displayMessage = (m) => console.log(m)
+
+/**
+ * Exports the displayMessage function.
+ */
+module.exports = displayMessage;
